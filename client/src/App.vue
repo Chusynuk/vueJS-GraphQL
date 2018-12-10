285289ddc2a7ae1071f225fee64adc5f03306eff
Chusynuk <recycled_loops@hotmail.com>
2018-12-10 18:34:49 +0100

    add client

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <home></home>
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <v-btn color="error">Button</v-btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: {}
+};
+</script>
