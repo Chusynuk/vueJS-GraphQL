01a01de6e9da37a321fa64a08eefd07ca4f96c1c
Chusynuk <recycled_loops@hotmail.com>
2018-12-11 17:25:18 +0100

    apollo-boost and vue-apollo

---
+++ client/src/components/Auth/Signin.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>Sign in</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Signin"
+};
+</script>
+
---
+++ client/src/components/Auth/Signup.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>Sign up</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Signup"
+};
+</script>
+
