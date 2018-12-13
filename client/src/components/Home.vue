3be2614c57ab31c09fcb180d8b0c6d2ec0f83a67
Chusynuk <recycled_loops@hotmail.com>
2018-12-13 17:19:59 +0100

    loading spinner

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <ApolloQuery :query="getPostsQuery">
-      <template slot-scope="{result: {loading, error, data }}">
-        <div v-if="loading">Loading...</div>
-        <div v-else-if="error">{{error.message}}</div>
-        <div v-else-if="networkStatus">{{networkStatus}}</div>
-        <ul v-else v-for="post in data.getPosts" :key="post._id">
-          <li>
-            {{post.title}}
-            {{post.imageUrl}}
-            {{post.likes}}
-          </li>
-        </ul>
-      </template>
-    </ApolloQuery>
-    <v-btn color="error">Button</v-btn>
-  </div>
+  <v-container text-xs-center>
+    <v-layout row>
+      <v-dialog v-model="loading" persistent fullscreen>
+        <v-container fill-height>
+          <v-layout row justify-center align-center>
+            <v-progress-circular indeterminate :size="70" :width="7" color="secondary"></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
+    <v-flex xs12>
+      <v-carousel
+        text-xs-center
+        v-if="!loading && posts.length > 0"
+        v-bind="{'cycle': true}"
+        interval="4000"
+      >
+        <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title">{{post.title}}</h1>
+          <h2></h2>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
+  </v-container>
 </template>
 
 <script>
@@ -25,20 +31,36 @@ import { gql } from 'apollo-boost'
 
 export default {
   name: 'home',
-  data() {
-    return {
-      getPostsQuery: gql`
-        query {
-          getPosts {
-            _id
-            title
-            description
-            imageUrl
-            likes
-          }
-        }
-      `,
-    }
+  created() {
+    this.handleGetCarouselPosts()
+  },
+  computed: {
+    posts() {
+      return this.$store.getters.posts
+    },
+    loading() {
+      return this.$store.getters.loading
+    },
+  },
+  methods: {
+    handleGetCarouselPosts() {
+      // reach out to Vuex store, fire action that gets posts for carousel
+      this.$store.dispatch('getPosts')
+    },
   },
 }
 </script>
+<style scoped>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
+
